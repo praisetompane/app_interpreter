f5b93244931bb11792bebf005dfca29b749e86c5
Praise Tompane <praisetompane@icloud.com>
2025-08-31 08:53:01 +0200

    fix: implement while loop

---
+++ README.md
@@ -0,0 +1,4 @@
+# app_interpreter
+![build status](https://github.com/praisetompane/app_interpreter/actions/workflows/app.yaml/badge.svg)
+
+A toy Postfix notation program interpreter.
--- src/app_interpreter/interpreter.py
+++ src/app_interpreter/interpreter.py
@@ -1,38 +1,57 @@
-import sys
 from collections import deque
-
+import argparse
 
 class Evaluator:
     def __init__(self) -> None:
         self.variables = {}
+        self._instruction_pointer = 0
 
     def evaluate_program(self, program_text):
-        lines = [line for line in program_text.split(
+        def extract_statement(text):
+            return text.split(maxsplit=1)[0]
+
+        instructions = [line for line in program_text.split(
             "\n") if line.strip() != ""]
-        instruction_pointer = 0
-        while instruction_pointer < len(lines):
-            instruction_register = lines[instruction_pointer]
-            statement = instruction_register.split(maxsplit=1)[0]
+
+        while self._instruction_pointer < len(instructions):
+            instruction_register = instructions[self._instruction_pointer]
+            statement = extract_statement(instruction_register)
             match statement:
-                case "while": pass
-                case "end": pass
-                case _:
+                case "while":
+                    _, expression = instruction_register.split(maxsplit=1)
+                    if self.evaluate_arithmetic(expression):
+                        # move to the body of while loop
+                        self._instruction_pointer += 1
+                    else:
+                        # skip the body of the loop
+                        while instructions[self._instruction_pointer] != "end":
+                            self._instruction_pointer += 1
+                        # move the instruction pointer to the next instruction after the loop keyword|statement
+                        self._instruction_pointer += 1
+                case "end":
+                        # circle back to the while loop statement|keyword
+                        # until its expressions evaluates to false and we move the instruction pointer past the while loop's end
+                        while extract_statement(instructions[self._instruction_pointer]) != "while":
+                            self._instruction_pointer -= 1
+                case _: # assume non while instructions are assignments or literal expressions
                     (variable, equal_symbol, expression) = instruction_register.split(maxsplit=2)
                     if equal_symbol == "=":
                         self.variables[variable] = self.evaluate_arithmetic(expression)
                     else:
-                        # store literals as identity key and value
+                        # store literals as an identity of key and value, where key = value
                         result = self.evaluate_arithmetic(instruction_register)
                         self.variables[result] = result
-            instruction_pointer += 1
+                    self._instruction_pointer += 1
 
-    def evaluate_arithmetic(self, expression):
+        return self.variables
+
+    def evaluate_arithmetic(self, arithmetic_expression):
         """Postfix notation arithmetic evaluation
 
         Args:
-            program_string (str): _description_
+            arithmetic_expression (str): Arithmetic expression ton evaluate.
         """
-        tokens = expression.split()
+        tokens = arithmetic_expression.split()
         stack = deque()
 
         for t in tokens:
@@ -60,7 +79,11 @@ class Evaluator:
 
 
 if __name__ == "__main__":
-    # program_file = open(sys.argv[1])
-    program_text = open("tests/app_interpreter/addition.txt").read()
+    parser = argparse.ArgumentParser("python src/app_interpreter/interpreter.py ./tests/resources/factorial.txt")
+    parser.add_argument("program_file")
+
+    args = parser.parse_args()
+    program_text = open(args.program_file).read()
+
     interpreter = Evaluator()
-    interpreter.evaluate_program(program_text)
+    print(interpreter.evaluate_program(program_text))
